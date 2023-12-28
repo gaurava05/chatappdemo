9ba2081549fd0e0f2a083662ea1a538c0858b55e
Gaurav Agarwal <gaagarwal@microsoft.com>
2023-12-28 15:41:21 +0530

    refactor styles and add socket.io

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import express from 'express';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import express from "express";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
 
 const app = express();
+const io = new Server(app);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-  
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  console.log(socket);
 });
 
 app.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
